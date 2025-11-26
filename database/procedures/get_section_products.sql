f68e981a8886d44b2bf7fb6c0417f31ce5efa114
Rija Ali <rijaali287@gmail.com>
2025-11-26 22:14:09 +0500

    updates in database procedures

--- database/procedures/get_section_products.sql
+++ database/procedures/get_section_products.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION niche_data.get_section_products(section_name TEXT)
+CReate OR REPLACE FUNCTION niche_data.get_section_products(p_section_name TEXT)
 RETURNS TABLE (
     article_id TEXT,
     prod_name TEXT,
@@ -13,9 +13,9 @@ RETURNS TABLE (
 BEGIN
     RETURN QUERY
     SELECT 
-        a.article_id,
-        a.prod_name,
-        a.price,
+        a.article_id::TEXT,
+        a.prod_name::TEXT,
+        a.price::NUMERIC,
         CASE 
             WHEN a.product_type_name = 'Hoodie' THEN 'hoodie'
             WHEN a.product_type_name = 'Jacket' THEN 'jacket'
@@ -26,8 +26,8 @@ BEGIN
             WHEN a.product_group_name = 'Accessories' THEN 'accessory'
             ELSE 'other'
         END AS category,
-        section_name,
-        a.stock,
+        p_section_name AS final_section,
+        a.stock::INT,
 
         -- Avg Rating
         (SELECT AVG(r.rating)::NUMERIC 
@@ -35,7 +35,7 @@ BEGIN
          WHERE r.article_id = a.article_id) AS average_rating,
 
         -- Total Reviews
-        (SELECT COUNT(*) FROM niche_data.reviews r 
+        (SELECT COUNT(*)::INT FROM niche_data.reviews r 
          WHERE r.article_id = a.article_id) AS total_reviews,
 
         -- Popularity
@@ -44,23 +44,23 @@ BEGIN
 
     FROM niche_data.articles a
     WHERE 
-        (section_name = 'men' AND (
+        (p_section_name = 'men' AND (
             a.section_name ILIKE '%Men%'
             OR a.section_name ILIKE '%Boy%'
             OR a.section_name IN ('Men Underwear','Men Shoes','Mens Outerwear','Men H&M Sport')
         ))
-        OR (section_name = 'women' AND (
+        OR (p_section_name = 'women' AND (
             a.section_name ILIKE '%Women%'
             OR a.section_name ILIKE '%Ladies%'
             OR a.section_name ILIKE 'H&M+%'
             OR a.section_name = 'Mama'
         ))
-        OR (section_name = 'kids' AND (
+        OR (p_section_name = 'kids' AND (
             a.section_name ILIKE '%Kids%'
             OR a.section_name ILIKE '%Baby%'
             OR a.section_name IN ('Young Girl','Young Boy','Kids Boy','Kids Girl')
         ))
-        OR (section_name = 'accessories' AND a.product_group_name = 'Accessories')
-        OR (section_name = 'unisex' AND a.product_group_name <> 'Accessories');
+        OR (p_section_name = 'accessories' AND a.product_group_name = 'Accessories')
+        OR (p_section_name = 'unisex' AND a.product_group_name <> 'Accessories');
 END;
 $$ LANGUAGE plpgsql STABLE;
--- database/procedures/get_sections.sql
+++ database/procedures/get_sections.sql
@@ -14,48 +14,48 @@ BEGIN
             'men' AS section_name,
             'Men' AS display_name,
             COUNT(*) AS total_products
-        FROM niche_data.articles
-        WHERE section_name ILIKE '%Men%'
-           OR section_name ILIKE '%Boy%'
-           OR section_name IN ('Men Underwear','Men Shoes','Mens Outerwear','Men H&M Sport')
+        FROM niche_data.articles a
+        WHERE a.section_name ILIKE '%Men%'
+           OR a.section_name ILIKE '%Boy%'
+           OR a.section_name IN ('Men Underwear','Men Shoes','Mens Outerwear','Men H&M Sport')
 
         UNION ALL
         SELECT
             2, 'women', 'Women',
-            COUNT(*) FROM niche_data.articles
-        WHERE section_name ILIKE '%Women%'
-           OR section_name ILIKE '%Ladies%'
-           OR section_name ILIKE 'H&M+%'
-           OR section_name = 'Mama'
+            COUNT(*) FROM niche_data.articles a2
+        WHERE a2.section_name ILIKE '%Women%'
+           OR a2.section_name ILIKE '%Ladies%'
+           OR a2.section_name ILIKE 'H&M+%'
+           OR a2.section_name = 'Mama'
 
         UNION ALL
         SELECT
             3, 'kids', 'Kids',
-            COUNT(*) FROM niche_data.articles
-        WHERE section_name ILIKE '%Kids%'
-           OR section_name ILIKE '%Baby%'
-           OR section_name IN ('Young Girl','Young Boy','Kids Boy','Kids Girl')
+            COUNT(*) FROM niche_data.articles a3
+        WHERE a3.section_name ILIKE '%Kids%'
+           OR a3.section_name ILIKE '%Baby%'
+           OR a3.section_name IN ('Young Girl','Young Boy','Kids Boy','Kids Girl')
 
         UNION ALL
         SELECT
             4, 'unisex', 'Unisex',
-            COUNT(*) FROM niche_data.articles
-        WHERE article_id NOT IN (
-            SELECT article_id FROM niche_data.articles WHERE
-                product_group_name = 'Accessories'
-                OR section_name ILIKE '%Men%'
-                OR section_name ILIKE '%Women%'
-                OR section_name ILIKE '%Kids%'
-                OR section_name ILIKE '%Boy%'
-                OR section_name ILIKE '%Girl%'
-                OR section_name ILIKE '%Baby%'
+            COUNT(*) FROM niche_data.articles a4
+        WHERE a4.article_id NOT IN (
+            SELECT a_sub.article_id FROM niche_data.articles a_sub WHERE
+                a_sub.product_group_name = 'Accessories'
+                OR a_sub.section_name ILIKE '%Men%'
+                OR a_sub.section_name ILIKE '%Women%'
+                OR a_sub.section_name ILIKE '%Kids%'
+                OR a_sub.section_name ILIKE '%Boy%'
+                OR a_sub.section_name ILIKE '%Girl%'
+                OR a_sub.section_name ILIKE '%Baby%'
         )
 
         UNION ALL
         SELECT
             5, 'accessories', 'Accessories',
-            COUNT(*) FROM niche_data.articles
-        WHERE product_group_name = 'Accessories'
+            COUNT(*) FROM niche_data.articles a5
+        WHERE a5.product_group_name = 'Accessories'
     ) AS s;
 END;
 $$ LANGUAGE plpgsql STABLE;
